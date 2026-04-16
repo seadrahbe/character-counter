b5ce67ae653eb22de93d242400ba2b1b14eb93bd
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-04-16 11:12:50 -0700

    Finished testing

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -4,4 +4,24 @@ import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
     
+  @Test
+  void testCharacterShowsUpMultipleTimesGivesCorrectFrequency() {
+    // Arrange
+    CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+    // Act
+    int actual = counter.getFrequency('l');
+    // Assert
+    assertEquals(2, actual);
+  } 
+
+  @Test
+  void testGetRelativePercentageReturns0ForMissingCharacter() {
+    // Arrange
+    CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+    // Act
+    double actual = counter.getRelativePercentage('x');
+    // Assert
+    assertEquals(0.0, actual, 0.0001);
+  }
+
 }
